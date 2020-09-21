601ab5a461dbac5cd6afd88ff2a7b2c129f83a8e
fildubek <fildubek@protonmail>
2020-09-21 19:14:00 +0200

    release

---
+++ Platforms.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Platforms" tilewidth="512" tileheight="512" tilecount="36" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Object_14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="512" height="256" source="Raw/free_flat_tileset/PNG/Object_15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="512" height="512" source="Raw/free_flat_tileset/PNG/Object_16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="512" height="512" source="Raw/free_flat_tileset/PNG/Object_17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="512" height="512" source="Raw/free_flat_tileset/PNG/Object_18.png"/>
+ </tile>
+ <tile id="18">
+  <image width="256" height="512" source="Raw/free_flat_tileset/PNG/Object_19.png"/>
+ </tile>
+ <tile id="19">
+  <image width="256" height="512" source="Raw/free_flat_tileset/PNG/Object_20.png"/>
+ </tile>
+ <tile id="20">
+  <image width="256" height="512" source="Raw/free_flat_tileset/PNG/Object_21.png"/>
+ </tile>
+ <tile id="21">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_3.png"/>
+ </tile>
+ <tile id="24">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_4.png"/>
+ </tile>
+ <tile id="25">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_5.png"/>
+ </tile>
+ <tile id="26">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_6.png"/>
+ </tile>
+ <tile id="27">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_7.png"/>
+ </tile>
+ <tile id="28">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_8.png"/>
+ </tile>
+ <tile id="29">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_9.png"/>
+ </tile>
+ <tile id="30">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_10.png"/>
+ </tile>
+ <tile id="31">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_11.png"/>
+ </tile>
+ <tile id="32">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_12.png"/>
+ </tile>
+ <tile id="33">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_13.png"/>
+ </tile>
+ <tile id="34">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_14.png"/>
+ </tile>
+ <tile id="35">
+  <image width="256" height="256" source="Raw/free_flat_tileset/PNG/Tile_15.png"/>
+ </tile>
+</tileset>
